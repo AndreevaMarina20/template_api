63227cb0bde14e529fd00375c0e5cd5d67d57104
Студент группы IS <is@sielom.local>
2025-04-24 17:35:49 +0500

    Магазин посмотрели

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const supabase = createClient('https://hsyundcrtyabhhgkslnk.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzeXVuZGNydHlhYmhoZ2tzbG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MTY1MDYsImV4cCI6MjA2MDk5MjUwNn0.hNJaR2pXaqWl76rxs6MY5_DnrezXujzuheFTPpB3Fkw')
+const {createClient} = require('@supabase/supabase-js')
+
 
 const express = require('express')
-const color = require('colorette')
+
 
 const app = express()
 const port = 3000
 
-app.get('/students', async (req, res) => {
+const supabase = createClient('https://nkdhqmirvgtvxzzonzwu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rZGhxbWlydmd0dnh6em9uend1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxNzQ2MDAsImV4cCI6MjA0Nzc1MDYwMH0.8kfT574gewUG-OGGWsCOGHB0wwGVTzsyPRz33aNk5wg')
+
+app.get('/products', async (req, res) => {
 
   const { data, error } = await supabase
-  .from('students')
+  .from('products')
   .select()
 
   res.json(data)
 })
 
-app.post('/students', (req, res) => {
-  res.send('Добавление студентов')
+app.post('/products', (req, res) => {
+  res.send('Добавление продуктов')
 })
 
-app.delete('/students/:id', async (req, res) => {
+app.delete('/products/:id', async (req, res) => {
 
   const response = await supabase
-  .from('students')
+  .from('products')
   .delete()
   .eq('id', req.params.id)
 
-  res.json('Студент с id = ' + req.params.id + 'удален!')
+  res.json('Продукт с id = ' + req.params.id + 'удален!')
 })
 
-app.put('/students', (req, res) => {
-  res.send('Изменение студента')
+app.put('/products', (req, res) => {
+  res.send('Изменение продукта')
 })
 
 app.listen(port, () => {
